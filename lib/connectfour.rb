ea4cf887bf21e3d2ea8694b9fd0106fc8f4972fb
Torrey Hogan <hogantorrey@gmail.com>
2018-11-19 15:13:40 -0800

    Fully functional connect four game

--- lib/connectfour.rb
+++ lib/connectfour.rb
@@ -1,18 +1,19 @@
 class Game
 	attr_accessor :board
 	def initialize()
-		# @board = [[nil,nil,nil,nil,nil,nil,nil],
-		# 					[nil,nil,nil,nil,nil,nil,nil],
-		# 					[nil,nil,nil,nil,nil,nil,nil],
-		# 					[nil,nil,nil,nil,nil,nil,nil],
-		# 					[nil,nil,nil,nil,nil,nil,nil],
-		# 					[nil,nil,nil,nil,nil,nil,nil]]
+		@player_win = nil
 		@board = [[nil,nil,nil,nil,nil,nil,nil],
 							[nil,nil,nil,nil,nil,nil,nil],
-							[nil,nil,nil,nil,"X",nil,nil],
-							[nil,nil,nil,"X",nil,nil,nil],
-							[nil,nil,"X",nil,nil,nil,nil],
-							[nil,"X",nil,nil,nil,nil,nil]]
+							[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil]]
+		# @board = [[nil,nil,nil,nil,nil,nil,nil],
+		# 					[nil,nil,nil,nil,nil,nil,nil],
+		# 					[nil,nil,nil,nil,"X",nil,nil],
+		# 					[nil,nil,nil,"X",nil,nil,nil],
+		# 					[nil,nil,"X",nil,nil,nil,nil],
+		# 					[nil,"X",nil,nil,nil,nil,nil]]
 							
 	end
 
@@ -30,15 +31,82 @@ class Game
 
 	def random_start
 		player = ["X", "O"]
-		player(rand(2))
+		player[rand(2)]
 	end
 
 	def win
 		current = [0,5]
-		win_recursion(current,false)
+		if win_recursion(current,false)
+			puts "#{@player_win} is the winner!" 
+			return true
+		else 
+			false
+		end
+
 	end
 
-	def win_direction(search, x,y,row,column, count)
+	def win_recursion(current, winner)
+		column = current[0]
+		row = current[1]
+		x = 0
+		y = 0
+		count = 0
+		#sets what to search for ie: x or o
+		if @board[row][column] == "X"
+			search = "X"
+		elsif @board[row][column] == "Y"
+			search = "Y"
+		else 
+			search = "nothing"
+		end
+		#decides which direction to check
+		if (column + 1) > 0 && (column + 1) < 7 && @board[row][column + 1] == search 
+			count += 1
+			x = 1
+			winner = win_direction(search,x,y,row,column,count)
+		end
+		if row - 1 >= 0 && @board[row - 1][column] == search && winner == false
+			count += 1
+			x = 0
+			y = -1
+			winner = win_direction(search,x,y,row,column,count)
+		end
+		if @board[row - 1][column + 1] == search && winner == false
+			count += 1
+			x = 1
+			y = -1
+			winner = win_direction(search,x,y,row,column,count)
+		end
+		if @board[row - 1][column - 1] == search && winner == false
+			x = -1
+			y = -1
+			count += 1
+			winner =  win_direction(search,x,y,row,column,count)
+		end
+
+		#checks for win or continues to next space
+		# puts "#{current} #{search} #{@board[row][column]}"
+		if winner == false
+			if current[0] == 6 && current[1] == 0
+				return false
+			elsif current[0] < 6
+				current[0] += 1
+				win_recursion(current,winner)
+			elsif current[0] == 6
+				current[1] -= 1
+				current[0] = 0
+				win_recursion(current,winner)
+			end
+		elsif winner == true
+			@player_win = search
+			return winner
+		else
+			
+		end
+
+	end
+
+		def win_direction(search, x,y,row,column, count)
 		column += x 
 		row += y
 		if count == 4
@@ -51,67 +119,36 @@ class Game
 		end
 	end
 
+	def turn(player)
+		puts "#{player}'s turn"
+		puts "enter which column you'd like to drop a piece 0 - 6: "
+		player_column = gets.chomp.to_i
+		until player_placement(player_column, player) && player_column >= 0 && player_column < 7
+			puts "enter a valid column"
+			player_column = gets.chomp.to_i
+		end
+	end
 
-	def win_recursion(current, winner)
-		puts "#{current} #{@board[current[1]][current[0]]}"
-		column = current[0]
-		row = current[1]
-		x = 0
-		y = 0
-		count = 0
-		#sets what to search for ie: x or o
-		if @board[row][column] == "X"
-			search = "X"
-			count = 1
-		elsif @board[row][column] == "Y"
-			search = "Y"
-			count = 1
-		else 
-			search = "nothing"
-		end
-		#decides which direction to check
-		if (column + 1) > 0 && (column + 1) < 7 && @board[row][column + 1] == search 
-			count += 1
-			x = 1
-			winner = win_direction(search,x,y,row,column,count)
-		end
-		if row - 1 >= 0 && @board[row - 1][column] == search
-			count += 1
-			x = 0
-			y = -1
-			winner = win_direction(search,x,y,row,column,count)
-		end
-		if @board[row - 1][column + 1] == search
-			count += 1
-			x = 1
-			y = -1
-			winner = true if win_direction(search,x,y,row,column,count)
-		end
-		if @board[row - 1][column + 1] == search
-			x = -1
-			y = -1
-			count += 1
-			winner = true if win_direction(search,x,y,row,column,count)
-		end
-
-		#checks for win or continues to next space
-		# puts "#{current} #{search} #{@board[row][column]}"
-		if winner != true
-			if current[0] == 6 && current[1] == 0
-				return false
-			elsif current[0] < 6
-				current[0] += 1
-				win_recursion(current,winner)
-			elsif current[0] == 6
-				current[1] -= 1
-				current[0] = 0
-				win_recursion(current,winner)
+	def player_placement(player_column, player)
+		row = 5
+		column = player_column
+		puts "#{player_column} #{player}"
+		while row >= 0
+			if @board[row][column] == nil
+				@board[row][column] = player.to_s
+				return true
+			else
+				row -= 1
 			end
-		elsif winner == true
-			puts "Winner!"
-			return winner
-		else
-			
+		end
+		return false
+	end
+
+	def full
+		@board.all? do |row|
+			row.all? do |el|
+				el != nil
+			end
 		end
 
 	end
@@ -120,22 +157,16 @@ class Game
 
 	def start
 		player = random_start
-		while win == false
+		until win || full
 			display_board
 			turn(player)
 			player == "X" ? player = "O" : player = "X"
 		end
 		display_board
-		puts "#{win} WON!!!!!"
 	end
 
 
 end
 
 game = Game.new
-
-# game.board[-1] = ["X","O","X","X","O","X","X"]
-# game.board[0] = ["X","X","X","X","X","X","X"]
-game.display_board
-puts game.win 
-game.display_board
+game.start
--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -1,17 +1,52 @@
 require './lib/connectfour'
 
 RSpec.describe Game do 
-	describe "#win" do 
-		it "return X if x wins the game" do 
+	describe "#win_recursion" do 
+		it "return true if bottom line win" do 
 			game = Game.new
-			game.board[-1] = ["X","X","X","X","X","X","X"]
-			expect(game.win).to eql("X")
+			game.board[2] = ["X","X","X","X","X","X","X"]
+			expect(game.win_recursion([0,5],false)).to eql(true)
+		end
+			it "return true if diagonal win" do 
+			game = Game.new
+			game.board =[[nil,nil,nil,nil,nil,nil,nil],
+									[nil,nil,nil,nil,nil,nil,nil],
+									[nil,nil,nil,nil,"X",nil,nil],
+									[nil,nil,nil,"X",nil,nil,nil],
+									[nil,nil,"X",nil,nil,nil,nil],
+									[nil,"X",nil,nil,nil,nil,nil]]
+			expect(game.win_recursion([0,5],false)).to eql(true)
+		end
+
+			it "return true if diagonal win" do 
+			game = Game.new
+			game.board =[[nil,nil,nil,nil,nil,nil,nil],
+									[nil,"X",nil,nil,nil,nil,nil],
+									[nil,nil,"X",nil,nil,nil,nil],
+									[nil,nil,nil,"X",nil,nil,nil],
+									[nil,nil,nil,nil,"X",nil,nil],
+									[nil,nil,nil,nil,nil,nil,nil]]
+			expect(game.win_recursion([0,5],false)).to eql(true)
 		end
 	end
 
-	describe "#turn" do
-		it "accept user input and update board"
-		game = Game.new
-		expect(game.turn).to eql()
+	describe "#full" do 
+		it "return true if board is full" do 
+			game = Game.new
+			game.board = [["X","X","X","X","X","X","X"],
+										["X","X","X","X","X","X","X"],
+										["X","X","X","X","X","X","X"],
+										["X","X","X","X","X","X","X"],
+										["X","X","X","X","X","X","X"],
+										["X","X","X","X","X","X","X"]]
+			expect(game.full).to eql(true)
+		end
 	end
+
+	# describe "#player_placement" do
+	# 	it "accept user input and update board"
+	# 	game = Game.new
+	# 	expect(game.player_placement(3)).to eql()
+	# end
+
 end
