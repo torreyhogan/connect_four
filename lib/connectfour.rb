73d361e88695b876703ef9ecb2b297ba5abc1518
Torrey Hogan <hogantorrey@gmail.com>
2018-11-18 19:04:44 -0800

    working on win search

---
+++ lib/connectfour.rb
@@ -0,0 +1,132 @@
+class Game
+	attr_accessor :board
+	def initialize()
+		@board = [[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil],
+							[nil,nil,nil,nil,nil,nil,nil]]
+							
+	end
+
+	def display_board
+		board = @board.dup 
+		board.each do |row|
+			string = ""
+			row.each do |space| 
+				space == nil ? x = "-" : x = space
+				string += "#{x} "
+			end
+			puts string
+		end
+	end
+
+	def random_start
+		player = ["X", "O"]
+		player(rand(2))
+	end
+
+	def win
+		current = [0,0]
+		win_recursion(current,false)
+	end
+
+	def win_direction(search, x,y,current, count)
+		current[0] += x
+		current[1] += y
+		if count == 4
+			return true
+		elsif @board[current[0],current[1]] == search
+			count += 1
+			win_direction(search, x,y, current, count)
+		else 
+			return false
+		end
+	end
+
+
+	def win_recursion(current, winner)
+		column = current[0]
+		row = current[1]
+		x = 0
+		y = 0
+		count = 0
+
+		#sets what to search for ie: x or o
+		if @board[column][row] == "X"
+			search = "X"
+			count = 1
+		elsif @board[column][row] == "Y"
+			search = "Y"
+			count = 1
+		else 
+			search = "nothing"
+		end
+		#decides which direction to check
+		if @board[column][row + 1] == search
+			count += 1
+			y += 1
+			winner = win_direction(search,x,y,current,count)
+		end
+		if @board[column+ 1][row] == search
+			count += 1
+			x = 1
+			y = 0
+			winner = win_direction(search,x,y,current,count)
+		end
+		if @board[column + 1][row + 1] == search
+			count += 1
+			x = 1
+			y = 1
+			winner = true if win_direction(search,x,y,current,count)
+		end
+		if @board[x - 1][y + 1] == search
+			x = -1
+			y = 1
+			count += 1
+			winner = true if win_direction(search,x,y,current,count)
+		end
+
+		#checks for win or continues to next space
+		if winner != true
+			if current[1] >= 6 && current[0] >= 7
+				return false
+			elsif current[1] <= 6
+				current[1] += 1
+				win_recursion(current,winner)
+			elsif current[1] <= 6
+				current[0] += 1
+				win_recursion(current,winner)
+			end
+		elsif winner == true
+			puts "Winner!"
+			return winner
+		else
+			
+		end
+
+	end
+
+		
+
+	def start
+		player = random_start
+		while win == false
+			display_board
+			turn(player)
+			player == "X" ? player = "O" : player = "X"
+		end
+		display_board
+		puts "#{win} WON!!!!!"
+	end
+
+
+end
+
+game = Game.new
+
+game.board[-1] = ["X","X","X","X","X","X","X"]
+game.display_board
+puts game.win 
--- spec/connectfour_spec.rb
+++ spec/connectfour_spec.rb
@@ -1,10 +1,17 @@
-require './lib/calculator'
+require './lib/connectfour'
 
 RSpec.describe Game do 
-	describe "#turn" do 
-		it "something" do 
+	describe "#win" do 
+		it "return X if x wins the game" do 
 			game = Game.new
-			expect().to eql()
+			game.board[-1] = ["X","X","X","X","X","X","X"]
+			expect(game.win).to eql("X")
 		end
+	end
+
+	describe "#turn" do
+		it "accept user input and update board"
+		game = Game.new
+		expect(game.turn).to eql()
 	end
 end
